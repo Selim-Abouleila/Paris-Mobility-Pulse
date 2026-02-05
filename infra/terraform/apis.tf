3bd3830de82a6b8d1ce34a11e53a3f095b37b36a
Selim-Abouleila <selimabouleila@gmail.com>
2026-02-05 13:46:49 +0100

    fix: enable iamcredentials API for OIDC token generation

--- infra/terraform/apis.tf
+++ infra/terraform/apis.tf
@@ -52,3 +52,8 @@ resource "google_project_service" "cloudscheduler" {
   service            = "cloudscheduler.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "iamcredentials" {
+  service            = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
